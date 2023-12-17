c1a4f52bd82ab02b90be530134f0284b05f3dbd0
aidandaly20 <dalybu@bc.edu>
2023-12-17 18:52:57 -0500

    finishing

--- src/Driver.java
+++ src/Driver.java
@@ -9,9 +9,10 @@ public class Driver {
                 System.out.println(
                                 "How this program works:\n\n To start - it's important to understand that this app maintains state so you can switch between commands at will.");
                 System.out.println(
-                                "If this is your first time running this program you should always run fetch first.");
+                                "If this is your first time running this program it may be helpful to have a list of players and teams up in another tab.");
+                System.out.println("Data are available from the years 1997-98 season through the 2022-23 season.");
                 System.out.println(
-                                "Compare - allows you to compare data that you have. You can compare data between teams. If you try to compare data that you don't have you will be asked to choose again.");
+                                "Compare - allows you to compare data. If you try to compare data that is not present you will be prompted to enter another entity.");
                 System.out.println("Quit - allows you to quit the program at any moment.");
 
                 Scanner scanner = new Scanner(System.in);
